d35b449e7883c67eceed0328af1d10d61aa8e18c
Muhammad Nadjrilillah <ajil7703@gmail.com>
2025-11-22 21:03:50 +0700

    Main Menu FMOD

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [SerializeField] EventReference menuMusic;
+    [SerializeField] EventReference gameplayMusic;
+
+    EventInstance currentMusic;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+    }
+
+    public void PlayMenuMusic()
+    {
+        if (currentMusic.isValid()) currentMusic.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+
+        currentMusic = RuntimeManager.CreateInstance(menuMusic);
+        currentMusic.start();
+    }
+
+    public void PlayGameplayMusic()
+    {
+        if (currentMusic.isValid()) currentMusic.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+
+        currentMusic = RuntimeManager.CreateInstance(gameplayMusic);
+        currentMusic.start();
+    }
+
+    public void PlayOneShot(EventReference sound, Vector3 pos)
+    {
+        RuntimeManager.PlayOneShot(sound, pos);
+    }
+}
